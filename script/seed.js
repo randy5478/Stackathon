b5222bafa5efa9d84af2fa45025b6ad9307af180
Randy Hyun <randyh5478@gmail.com>
2021-01-22 19:18:37 -0600

    feat(seed.js): added dummy data

--- script/seed.js
+++ script/seed.js
@@ -1,18 +1,40 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {AllStats} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+  const allStats = await Promise.all([
+    AllStats.create({
+      HP: 8,
+      Energy: 8,
+      Wisdom: 2,
+      Speed: 1,
+      Strength: 1,
+      Date: '2021-1-20'
+    }),
+    AllStats.create({
+      HP: 7,
+      Energy: 6,
+      Wisdom: 1,
+      Speed: 1,
+      Strength: 1,
+      Date: '2021-1-21'
+    }),
+    AllStats.create({
+      HP: 0,
+      Energy: 0,
+      Wisdom: 0,
+      Speed: 0,
+      Strength: 0,
+      Date: '2021-1-22'
+    })
   ])
 
-  console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${allStats.length} users`)
   console.log(`seeded successfully`)
 }
 
