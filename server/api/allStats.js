9d6c026e699baa72ad29c19e23f7b984f5743ba7
Randy Hyun <randyh5478@gmail.com>
2021-01-22 19:22:31 -0600

    feat(allStats Routes): added all routes for allStats page

---
+++ server/api/allStats.js
@@ -0,0 +1,47 @@
+const router = require('express').Router()
+const {AllStats} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allStats = await AllStats.findAll()
+    res.json(allStats)
+  } catch (err) {
+    next(err)
+  }
+})
+router.get('/:date', async (req, res, next) => {
+  try {
+    const allStats = await AllStats.findOne({
+      where: {
+        Date: req.params.date //e.i 2021-01-21
+      }
+    })
+    res.json(allStats)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    await AllStats.create(req.body)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:date', async (req, res, next) => {
+  try {
+    console.log('Params--->', req.params.date)
+    console.log('Body--->', req.body)
+    const updateAllStats = await AllStats.findOne({
+      where: {
+        Date: req.params.date
+      }
+    })
+    await updateAllStats.update(req.body)
+  } catch (err) {
+    next(err)
+  }
+})
--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/allStats', require('./allStats'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
